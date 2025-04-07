7cdf9fb017ed03f0ea6eaad61344c97bae96e752
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-04-07 11:02:56 +0200

    arduino-cli-install: Removed OS specific scripts.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- arduino-cli-install.sh
+++
@@ -1,5 +0,0 @@
-# Get the version from the command line arguments
-ard_cli_version= $1
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh -s $ard_cli_version
-sudo mv bin/arduino-cli /usr/local/bin/arduino-cli
-arduino-cli version
